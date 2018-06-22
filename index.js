359c40d9b0b360ddc194922f829baf7bd3bc410c
Clayton Chevrier <claytonchevrier@gmail.com>
2018-06-22 11:48:53 -0400

    Initial release

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+	get_date_string: function() {
+		return "It worked.";
+	}
+};
---
+++ test.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var get_date_string = require('ucg-rb-common').get_date_string;
+
+print get_date_string();
